c5de8ee94da14aebdb373d052cc73c90cacb3b07
Monk_Urukhai <awfulwafful@gmail.com>
2020-03-26 17:19:06 -0600

    update to description and functionality

--- ElementalDischargeRemoval/ElementalDishargeRemoval.cs
+++ ElementalDischargeRemoval/ElementalDishargeRemoval.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections;
+using System.Reflection;
 // this namespace is used for modloader
 using Partiality.Modloader;
 using UnityEngine;
+using Localizer;
 
 // the name of this mod
 namespace ElementalDischargeRemoval
@@ -54,6 +56,9 @@ namespace ElementalDischargeRemoval
             // set up the instance
             public static ElementalDischargeRemovalScript Instance;
 
+            // define flags for reflection
+            public BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static;
+
             // set up Awake for prefab loader
             internal void Awake()
             {
@@ -68,6 +73,49 @@ namespace ElementalDischargeRemoval
                 }
 
                 // Prefabs are loaded
+
+                // Elemental Discharge Object ID 8200310
+                var _elementalDischargeID = 8200310;
+
+                // check to make sure it is a skill
+                if (ResourcesPrefabManager.Instance.GetItemPrefab(_elementalDischargeID) is Skill _skill)
+                {
+                    // maintain compatibility with other mods by checking if unqique effects added
+                    if (_skill.transform.Find("Effects") is Transform effects)
+                    {
+                        // add component to child
+                        effects.gameObject.AddComponent<RemoveImbueEffects>();
+                    }
+                    else
+                    {
+                        // create new child
+                        var removeImbueEffect = new GameObject("Effects");
+                        removeImbueEffect.transform.parent = _skill.transform;
+                        // add component to the child
+                        removeImbueEffect.AddComponent<RemoveImbueEffects>();
+                    }
+
+                    // change item description (copied from sinai's side loader)
+                   // string name = "Elemental Discharge";
+                    string desc = String.Format("Required: Infused Weapon\n\nThrust your weapon forward, removing the elemental infusion to shoot a projectile of that element.");
+
+                    ItemLocalization loc = new ItemLocalization(name, desc);
+
+                    //typeof(Item).GetField("m_name",flags).SetValue(_skill, name);
+
+                    if (typeof(LocalizationManager).GetField("m_itemLocalization", flags).GetValue(LocalizationManager.Instance) is Dictionary<int, ItemLocalization> dict)
+                    {
+                        if (dict.ContainsKey(_skill.ItemID))
+                        {
+                            dict[_skill.ItemID] = loc;
+                        }
+                        else
+                        {
+                            dict.Add(_skill.ItemID, loc);
+                        }
+                        typeof(LocalizationManager).GetField("m_itemLocalization", flags).SetValue(LocalizationManager.Instance, dict);
+                    }
+                }
             }
         }
     }
